8186ae88297abca2c4fef78f8a6aab364a2854aa
A'Darius Craig <adariusc03@gmail.com>
2022-08-29 18:37:02 -0500

    Added Background color, lines in road, and key speed contols.

---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpeedRacers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            mainLine(gamespeed);
+        }
+
+        // Shows the speed of the pic box white lines moving
+        void mainLine(int speed)
+        {
+
+            if (pictureBox1.Top >= 500)
+            {
+                pictureBox1.Top = 0;
+            }
+            else
+            {
+                pictureBox1.Top += speed;
+            }
+
+
+            if (pictureBox2.Top >= 500)
+            {
+                pictureBox2.Top = 0;
+            }
+            else
+            {
+                pictureBox2.Top += speed;
+            }
+
+
+            if (pictureBox3.Top >= 500)
+            {
+                pictureBox3.Top = 0;
+            }
+            else
+            {
+                pictureBox3.Top += speed;
+            }
+
+
+            if (pictureBox4.Top >= 500)
+            {
+                pictureBox4.Top = 0;
+            }
+            else
+            {
+                pictureBox4.Top += speed;
+            }
+
+
+            if (pictureBox5.Top >= 500)
+            {
+                pictureBox5.Top = 0;
+            }
+            else
+            {
+                pictureBox5.Top += speed;
+            }
+
+
+            if (pictureBox8.Top >= 500)
+            {
+                pictureBox8.Top = 0;
+            }
+            else
+            {
+                pictureBox8.Top += speed;
+            }
+        }
+
+        
+
+        private void Car1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+
+
+        int gamespeed = 0;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.Left)
+            {
+                if (Car1.Left > 0)
+                {
+                    Car1.Left += -8;
+                }
+
+            }
+
+            if (e.KeyCode == Keys.Right)
+            { // 380 is the border control for the car . 400 is the screen size
+                if (Car1.Right < 380)
+                    Car1.Left += 8;
+            }
+
+
+            if (e.KeyCode == Keys.Up)
+            {
+                if (gamespeed < 21)
+                {
+                    gamespeed++;
+                }
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                if (gamespeed > 0)
+                {
+                    gamespeed--;
+                }
+            }
+        }
+    }
+
+
+}
