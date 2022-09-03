01d79e41e9901efc23298dddfaceffc0dc204c85
A'Darius Craig <adariusc03@gmail.com>
2022-09-02 22:59:37 -0500

    Added game menu, restart and quit feature. Enemy(s) and fuel(points).

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,6 +40,18 @@
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.Car1 = new System.Windows.Forms.PictureBox();
+            this.enemy1 = new System.Windows.Forms.PictureBox();
+            this.enemy2 = new System.Windows.Forms.PictureBox();
+            this.enemy3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.point2 = new System.Windows.Forms.PictureBox();
+            this.point1 = new System.Windows.Forms.PictureBox();
+            this.point4 = new System.Windows.Forms.PictureBox();
+            this.point3 = new System.Windows.Forms.PictureBox();
+            this.game_Menu = new System.Windows.Forms.GroupBox();
+            this.over = new System.Windows.Forms.Label();
+            this.lbl_restart = new System.Windows.Forms.Label();
+            this.lbl_quit = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -49,6 +61,14 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Car1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.point2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.point1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.point4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.point3)).BeginInit();
+            this.game_Menu.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -134,13 +154,137 @@
             // Car1
             // 
             this.Car1.Image = ((System.Drawing.Image)(resources.GetObject("Car1.Image")));
-            this.Car1.Location = new System.Drawing.Point(54, 315);
+            this.Car1.Location = new System.Drawing.Point(122, 400);
             this.Car1.Name = "Car1";
-            this.Car1.Size = new System.Drawing.Size(46, 53);
+            this.Car1.Size = new System.Drawing.Size(46, 61);
             this.Car1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Car1.TabIndex = 9;
             this.Car1.TabStop = false;
             this.Car1.Click += new System.EventHandler(this.Car1_Click);
+            // 
+            // enemy1
+            // 
+            this.enemy1.Image = ((System.Drawing.Image)(resources.GetObject("enemy1.Image")));
+            this.enemy1.Location = new System.Drawing.Point(15, 208);
+            this.enemy1.Name = "enemy1";
+            this.enemy1.Size = new System.Drawing.Size(62, 61);
+            this.enemy1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy1.TabIndex = 10;
+            this.enemy1.TabStop = false;
+            // 
+            // enemy2
+            // 
+            this.enemy2.Image = ((System.Drawing.Image)(resources.GetObject("enemy2.Image")));
+            this.enemy2.Location = new System.Drawing.Point(274, 92);
+            this.enemy2.Name = "enemy2";
+            this.enemy2.Size = new System.Drawing.Size(62, 61);
+            this.enemy2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy2.TabIndex = 11;
+            this.enemy2.TabStop = false;
+            // 
+            // enemy3
+            // 
+            this.enemy3.Image = ((System.Drawing.Image)(resources.GetObject("enemy3.Image")));
+            this.enemy3.Location = new System.Drawing.Point(274, 349);
+            this.enemy3.Name = "enemy3";
+            this.enemy3.Size = new System.Drawing.Size(62, 61);
+            this.enemy3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy3.TabIndex = 12;
+            this.enemy3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MediumAquamarine;
+            this.label1.Location = new System.Drawing.Point(12, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Points:";
+            // 
+            // point2
+            // 
+            this.point2.Image = ((System.Drawing.Image)(resources.GetObject("point2.Image")));
+            this.point2.Location = new System.Drawing.Point(227, 223);
+            this.point2.Name = "point2";
+            this.point2.Size = new System.Drawing.Size(40, 46);
+            this.point2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.point2.TabIndex = 14;
+            this.point2.TabStop = false;
+            // 
+            // point1
+            // 
+            this.point1.Image = ((System.Drawing.Image)(resources.GetObject("point1.Image")));
+            this.point1.Location = new System.Drawing.Point(57, 308);
+            this.point1.Name = "point1";
+            this.point1.Size = new System.Drawing.Size(40, 46);
+            this.point1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.point1.TabIndex = 15;
+            this.point1.TabStop = false;
+            // 
+            // point4
+            // 
+            this.point4.Image = ((System.Drawing.Image)(resources.GetObject("point4.Image")));
+            this.point4.Location = new System.Drawing.Point(216, 35);
+            this.point4.Name = "point4";
+            this.point4.Size = new System.Drawing.Size(40, 46);
+            this.point4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.point4.TabIndex = 16;
+            this.point4.TabStop = false;
+            // 
+            // point3
+            // 
+            this.point3.Image = ((System.Drawing.Image)(resources.GetObject("point3.Image")));
+            this.point3.Location = new System.Drawing.Point(76, 136);
+            this.point3.Name = "point3";
+            this.point3.Size = new System.Drawing.Size(40, 46);
+            this.point3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.point3.TabIndex = 17;
+            this.point3.TabStop = false;
+            // 
+            // game_Menu
+            // 
+            this.game_Menu.Controls.Add(this.lbl_quit);
+            this.game_Menu.Controls.Add(this.lbl_restart);
+            this.game_Menu.Controls.Add(this.over);
+            this.game_Menu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.game_Menu.Location = new System.Drawing.Point(57, 136);
+            this.game_Menu.Name = "game_Menu";
+            this.game_Menu.Size = new System.Drawing.Size(269, 154);
+            this.game_Menu.TabIndex = 18;
+            this.game_Menu.TabStop = false;
+            this.game_Menu.Text = "Menu";
+            // 
+            // over
+            // 
+            this.over.AutoSize = true;
+            this.over.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.over.Location = new System.Drawing.Point(27, 28);
+            this.over.Name = "over";
+            this.over.Size = new System.Drawing.Size(209, 37);
+            this.over.TabIndex = 0;
+            this.over.Text = "GAMEOVER";
+            // 
+            // lbl_restart
+            // 
+            this.lbl_restart.AutoSize = true;
+            this.lbl_restart.Location = new System.Drawing.Point(99, 85);
+            this.lbl_restart.Name = "lbl_restart";
+            this.lbl_restart.Size = new System.Drawing.Size(69, 20);
+            this.lbl_restart.TabIndex = 1;
+            this.lbl_restart.Text = "Restart";
+            this.lbl_restart.Click += new System.EventHandler(this.lbl_restart_Click_1);
+            // 
+            // lbl_quit
+            // 
+            this.lbl_quit.AutoSize = true;
+            this.lbl_quit.Location = new System.Drawing.Point(99, 131);
+            this.lbl_quit.Name = "lbl_quit";
+            this.lbl_quit.Size = new System.Drawing.Size(42, 20);
+            this.lbl_quit.TabIndex = 2;
+            this.lbl_quit.Text = "Quit";
+            this.lbl_quit.Click += new System.EventHandler(this.lbl_quit_Click);
             // 
             // Form1
             // 
@@ -148,6 +292,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
             this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.game_Menu);
+            this.Controls.Add(this.point3);
+            this.Controls.Add(this.point4);
+            this.Controls.Add(this.point1);
+            this.Controls.Add(this.point2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.enemy3);
+            this.Controls.Add(this.enemy2);
+            this.Controls.Add(this.enemy1);
             this.Controls.Add(this.Car1);
             this.Controls.Add(this.pictureBox7);
             this.Controls.Add(this.pictureBox8);
@@ -159,6 +312,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
@@ -169,7 +323,17 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Car1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.point2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.point1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.point4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.point3)).EndInit();
+            this.game_Menu.ResumeLayout(false);
+            this.game_Menu.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -185,6 +349,18 @@
         private System.Windows.Forms.PictureBox pictureBox7;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.PictureBox Car1;
+        private System.Windows.Forms.PictureBox enemy1;
+        private System.Windows.Forms.PictureBox enemy2;
+        private System.Windows.Forms.PictureBox enemy3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox point2;
+        private System.Windows.Forms.PictureBox point1;
+        private System.Windows.Forms.PictureBox point4;
+        private System.Windows.Forms.PictureBox point3;
+        private System.Windows.Forms.GroupBox game_Menu;
+        private System.Windows.Forms.Label lbl_quit;
+        private System.Windows.Forms.Label lbl_restart;
+        private System.Windows.Forms.Label over;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,10 @@ namespace SpeedRacers
         public Form1()
         {
             InitializeComponent();
+            // over.Visible = false;
+            game_Menu.Hide();
+
+
         }
 
         private void pictureBox6_Click(object sender, EventArgs e)
@@ -27,9 +31,18 @@ namespace SpeedRacers
 
         }
 
+
+
+
         private void timer1_Tick(object sender, EventArgs e)
         {
             mainLine(gamespeed);
+            enemy(gamespeed);
+            gameOver();
+            point(gamespeed);
+            collection();
+
+
         }
 
         // Shows the speed of the pic box white lines moving
@@ -96,7 +109,145 @@ namespace SpeedRacers
             }
         }
 
-        
+        // method for the enemy cars speed and location
+        Random r = new Random();
+        int x;
+        void enemy(int speed)
+        {
+            if (enemy1.Top >= 500)
+            {
+                x = r.Next(0, 250);
+
+                enemy1.Location = new Point(x, 0);
+            }
+            else
+            {
+                enemy1.Top += speed;
+            }
+
+            if (enemy2.Top >= 500)
+            {
+                x = r.Next(0, 380);
+
+                enemy2.Location = new Point(x, 0);
+            }
+            else
+            {
+                enemy2.Top += speed;
+            }
+
+            if (enemy3.Top >= 500)
+            {
+                x = r.Next(200, 320);
+
+                enemy3.Location = new Point(x, 0);
+            }
+            else
+            {
+                enemy3.Top += speed;
+            }
+
+        }
+
+
+
+        // Points to collect
+
+
+        void point(int speed)
+        {
+            if (point4.Top >= 500)
+            {
+                x = r.Next(0, 380);
+                point4.Location = new Point(x, 0);
+            }
+            else
+            {
+                point4.Top += speed;
+            }
+
+            if (point3.Top >= 500)
+            {
+                x = r.Next(0, 380);
+                point3.Location = new Point(x, 0);
+            }
+            else
+            {
+                point3.Top += speed;
+            }
+
+            if (point2.Top >= 500)
+            {
+                x = r.Next(0, 380);
+                point2.Location = new Point(x, 0);
+            }
+            else
+            {
+                point2.Top += speed;
+            }
+
+            if (point1.Top >= 500)
+            {
+                x = r.Next(220, 350);
+                point1.Location = new Point(x, 0);
+            }
+            else
+            {
+                point1.Top += speed;
+            }
+        }
+
+        // Method to collect points
+        int collectp = 0;
+
+        void collection()
+        {
+            if (Car1.Bounds.IntersectsWith(point4.Bounds))
+            {
+                collectp++;
+                label1.Text = "Points:" + collectp.ToString();
+                x = r.Next(50, 300);
+
+                point4.Location = new Point(x, 0);
+
+
+            }
+
+            if (Car1.Bounds.IntersectsWith(point3.Bounds))
+            {
+                collectp++;
+                label1.Text = "Points:" + collectp.ToString();
+                x = r.Next(50, 300);
+
+                point3.Location = new Point(x, 0);
+
+
+            }
+
+            if (Car1.Bounds.IntersectsWith(point2.Bounds))
+            {
+                collectp++;
+                label1.Text = "Points:" + collectp.ToString();
+                x = r.Next(50, 300);
+
+                point2.Location = new Point(x, 0);
+
+
+            }
+
+            if (Car1.Bounds.IntersectsWith(point1.Bounds))
+            {
+                collectp++;
+                label1.Text = "Points:" + collectp.ToString();
+                x = r.Next(50, 300);
+
+                point1.Location = new Point(x, 0);
+
+
+            }
+        }
+
+
 
         private void Car1_Click(object sender, EventArgs e)
         {
@@ -106,11 +257,11 @@ namespace SpeedRacers
 
 
 
-
+        // gamespeed and controls
         int gamespeed = 0;
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            
+
             if (e.KeyCode == Keys.Left)
             {
                 if (Car1.Left > 0)
@@ -127,13 +278,17 @@ namespace SpeedRacers
             }
 
 
+            // background speed can increase with one push or held down
+
             if (e.KeyCode == Keys.Up)
             {
-                if (gamespeed < 21)
+                if (gamespeed < 20)
                 {
                     gamespeed++;
                 }
             }
+
+            // slows down the background speed when pressed or held down
 
             if (e.KeyCode == Keys.Down)
             {
@@ -143,7 +298,67 @@ namespace SpeedRacers
                 }
             }
         }
+
+
+        // game over when touhing enemy car
+        // i want timer to stop once enemy is hit for a game over
+        // game_Menu.Show() method helping bring up game menu
+
+        void gameOver()
+        {
+            if (Car1.Bounds.IntersectsWith(enemy1.Bounds))
+            {
+                timer1.Enabled = false;
+                over.Visible = true;
+                game_Menu.Show();
+            }
+
+            if (Car1.Bounds.IntersectsWith(enemy2.Bounds))
+            {
+                timer1.Enabled = false;
+                over.Visible = true;
+                game_Menu.Show();
+            }
+
+            if (Car1.Bounds.IntersectsWith(enemy3.Bounds))
+            {
+                timer1.Enabled = false;
+                over.Visible = true;
+                game_Menu.Show();
+            }
+        }
+
+       
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+        
+        
+        // Restarts the application when clicked
+
+        private void lbl_restart_Click_1(object sender, EventArgs e)
+        {
+            timer1.Start();
+            game_Menu.Hide();
+            label1.Text = "Points: 0";
+            Car1.Location = new Point(56, 381);
+            enemy1.Location = new Point(40, 177);
+            enemy2.Location = new Point(281, 282);
+            enemy3.Location = new Point(204, 23);
+            point1.Location = new Point(302, 164);
+            point2.Location = new Point(216, 337);
+            point3.Location = new Point(115, 282);
+            point4.Location = new Point(72, 38);
+        }
+            // Enables the application to exit when clicked
+        private void lbl_quit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 
 
 }
+
